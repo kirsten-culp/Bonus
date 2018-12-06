9b53ddd5c453c6a3eb86d1197630caedcc983bf0
kirsten-culp <43148594+kirsten-culp@users.noreply.github.com>
2018-12-05 19:28:03 -0500

    6

---
+++ Bonus_2/Bonus2/Assets/Scripts/NPCMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class NPCMove : MonoBehaviour {
+
+    [SerializeField]
+    Transform _destination;
+
+    NavMeshAgent _navMeshAgent;
+
+	// Use this for initialization
+	void Start () {
+        _navMeshAgent = this.GetComponent<NavMeshAgent>();
+
+        if(_navMeshAgent == null)
+        {
+            Debug.LogError("Not attached");
+        } else
+        {
+            SetDestination();
+        }
+
+    }
+
+    private void SetDestination()
+    {
+        if(_destination != null)
+        {
+            Vector3 targetVector = _destination.transform.position;
+            _navMeshAgent.SetDestination(targetVector);
+        }
+    }
+}
